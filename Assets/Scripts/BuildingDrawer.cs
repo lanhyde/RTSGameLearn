6d6cba0a713ef74875fda13564264453ddffc813
lan hyde <lanhaide@hotmail.com>
2020-05-22 00:59:43 +0900

    building drawer, build, and ordering

---
+++ Assets/Scripts/BuildingDrawer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace PromiseCode.RTS
+{
+    public class BuildingDrawer : MonoBehaviour
+    {
+        [SerializeField]
+        Renderer[] renderers;
+        static readonly int tintColor = Shader.PropertyToID("_TintColor");
+
+        public void SetBuildingAllowedState(bool isAllowed)
+        {
+            SetMeshColor(isAllowed ? Color.green : Color.red);
+        }
+
+        void SetMeshColor(Color color)
+        {
+            for(int i = 0; i < renderers.Length; ++i)
+            {
+                renderers[i].material.SetColor(tintColor, color);
+            }
+        }
+    }
+
+}
