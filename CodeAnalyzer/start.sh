98233b9e5305d675eee6ebf05578a124104fa91e
Haide Lan <lanhaide@hotmail.com>
2021-02-26 17:55:05 +0900

    Update start.sh

--- CodeAnalyzer/start.sh
+++ CodeAnalyzer/start.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
+git_diff_with_abs_path() {
+    local path
+
+    path=$(git rev-parse --show-toplevel) &&
+   git diff origin/develop...HEAD --name-only | grep -E ".*(\.cs)$" | sed "s,^,$path/,"
+}
+
 DOT_NET=$(which dotnet)
 CODE_ANALYZER="$(pwd -P)/CSharpCodeAnalyzer5.dll"
 RESULT_FILE_CACHE="$(pwd -P)/result.txt"
 RESULT=0
-DIFF=`git diff origin/develop...HEAD --name-only | grep -E ".*(\.cs)$"`
+DIFF=$(git_diff_with_abs_path)
+
 for file in $DIFF; do
 	if [ ! -e "$CODE_ANALYZER" ]; then
 	    echo "CodeAnalyzer not exist!"
 	    exit 0
     fi
-    RESULT="$DOTNET $CODE_ANALYZER" "$file" >> $RESULT_FILE_CACHE
+    ("$DOT_NET" "$CODE_ANALYZER" "$file") >> $RESULT_FILE_CACHE
+	RESULT=$((RESULT+$?))
 done
 
 if [ $RESULT -ne 0 ]; then
@@ -18,6 +27,4 @@ if [ $RESULT -ne 0 ]; then
 	cat $RESULT_FILE_CACHE
 fi
 
-echo "Finish"
 exit $RESULT
-
